ee492864939780e8038c5d209e664ace03e38b4f
Sascha Depold <sascha@depold.com>
2009-09-24 09:34:01 +0200

    fixed wrong path

--- examples/php/puzzle/index.php
+++ examples/php/puzzle/index.php
@@ -1,5 +1,5 @@
 <?php
-  include("/Users/prototype/Projects/dawanda-api/lib/php/v1/dawanda.php");
+  include("../../../lib/php/v1/dawanda.php");
   $api = new DaWandaAPI("380d7924396f5596116f3d8815c97dfd8c975582", "de");
 ?>
 
