035ac76900d3b58a4fc34ebd703d8aebdcd77f59
Sascha Depold <sascha@depold.com>
2010-02-19 13:37:58 +0100

    removed old files

--- examples/php/oauth/authorize.php
+++
@@ -1,8 +0,0 @@
-<?
-  require("config.php");
-  require("lib/OAuth.php");
-  include("../../../lib/php/v1/src/dawanda.php");
-  
-  $dawanda_oauth = new DaWandaOAuth($key, $secret, "de");
-  $dawanda_oauth->requestAuthorization("index.php");
-?>
