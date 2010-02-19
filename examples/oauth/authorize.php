bba21761b1393dbffc3e607f0c1ff68d4b000f3e
Sascha Depold <sascha@depold.com>
2010-02-19 13:39:33 +0100

    files from php client repo

---
+++ examples/oauth/authorize.php
@@ -0,0 +1,8 @@
+<?
+  require("config.php");
+  require("lib/OAuth.php");
+  include("../../lib/v1/src/dawanda.php");
+  
+  $dawanda_oauth = new DaWandaOAuth($key, $secret, "de");
+  $dawanda_oauth->requestAuthorization("index.php");
+?>
