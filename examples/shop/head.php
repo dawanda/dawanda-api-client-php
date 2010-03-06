37c7eabb5ecef996a1f3d9737b97d68a364784af
Sascha Depold <sascha@depold.com>
2010-03-06 22:22:07 +0100

    new example => shop (port of the javascript example)

---
+++ examples/shop/head.php
@@ -0,0 +1,104 @@
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<style type="text/css" media="screen">
+  body {
+    font-family: Verdana;
+    font-size: 12px;
+  }
+  
+  #mainContainer {
+    border: 2px solid #aaa;
+    background-color: #eeeeee;
+    padding: 10px;
+    -moz-border-radius: 10px;
+    -webkit-border-radius: 10px;
+  }
+
+  form {
+    border: 2px solid #aaa;
+    padding: 10px;
+    -moz-border-radius: 5px;
+    -webkit-border-radius: 5px;
+    background-color: #fafafa;
+  }
+
+  #category_container {
+    width: 250px;
+    float: left;
+    padding: 10px;
+    border: 2px solid #aaa;
+    background-color: #fafafa;
+    -moz-border-radius: 5px;
+    -webkit-border-radius: 5px;
+  }
+  
+  #category_container ul {
+    list-style-type: none;
+    margin: 0px;
+    padding: 0px;
+    border-bottom: none;
+  }
+  
+  #category_container li {
+    padding: 5px;
+    border-bottom: 2px solid #aaa;
+    text-align: center;
+    color: #555;
+  }
+  
+  #category_container li:hover {
+    background-color: #eee;
+    cursor: pointer;
+  }
+  
+  #category_container a {
+    display: block;
+  }
+  
+  #product_container {
+    margin-left: 290px;
+    padding: 10px;
+    border: 2px solid #aaa;
+    background-color: #fafafa;
+    -moz-border-radius: 5px;
+    -webkit-border-radius: 5px;
+  }
+  
+  #products {
+    margin: 0px;
+    padding: 0px;
+    margin-left: 20px;
+  }
+  
+  #products li {
+    width: 160px;
+    height: 190px;
+    display: block;
+    float: left;
+    padding: 0px 3px;
+    margin-right: 10px;
+    font-size: 12px;
+    color: #555;
+  }
+  
+  #products img {
+    border: 2px solid #aaa;
+    -moz-border-radius: 2px;
+    -webkit-border-radius: 2px;
+    padding: 2px;
+  }
+  
+  #pages {
+    color: #555;
+    float: right;
+    margin: 15px;
+  }
+  
+  #pagination_container {
+    padding-bottom: 10px;
+  }
+  
+  #pagination_container a {
+    font-weight: bold;
+    color: #777;
+  }
+</style>
