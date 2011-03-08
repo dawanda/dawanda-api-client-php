29c447eee2452204b22f38eac2dca7e66ae7b1a2
sdepold <sascha@dawanda.com>
2011-03-08 16:50:22 +0100

    added api example

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php echo "Hello world!"; ?>
+<html>
+  <head><? require("head.php"); ?></head>
+  <body>
+    <div id="mainContainer">
+      <div id="pages">
+        Page:
+        <span id="currentPage">0</span> / 
+        <span id="maxPage">0</span>
+      </div>
+      <form action="show.php" method="GET">
+        Username:
+        <input type="text" name="username"></input>
+        <input type="submit" value="Go!"></input>
+      </form>
+
+      <div id="container">
+        <div id="category_container"><ul id="categories"><li>Currently no data!</li></ul></div>
+        <div id="product_container">
+          <ul id="products"><li>Currently no data!</li></ul>
+          <div style="clear: both"></div>
+        </div>
+      </div>
+      <div style="clear: both"></div>
+    </div>
+  </body>
+</html>
